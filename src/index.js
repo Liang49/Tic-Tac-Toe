d34c151105d928fdfec0120256b2e7708444e546
Liang49 <liangzhang2014@gmail.com>
2019-09-29 17:00:14 -0400

    click

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ import "./styles.css";
 var player1 = "O";
 var player2 = "X";
 
-function handleClick() {
-  console.log(player1);
-  board.innerHTML = player1;
+function handleClick(e) {
+  count++;
+  console.log(count);
+  document.getElementById(e.target.id).innerHTML = player1;
 }
-var board = document.getElementsByClassName("board")[0];
 
-board.addEventListener("click", handleClick);
+var count = 0;
+
+var board = document.querySelectorAll(".board");
+for (let i = 0; i < board.length; i++) {
+  board[i].addEventListener("click", handleClick);
+  /*board[i].addEventListener("click", function() {
+    board[i].innerHTML = player1;
+    if (board[i].innerHTML === player1) {
+      count++;
+      console.log(count);
+    }
+    if (count % 2 === 0) {
+      board[i].innerHTML = player2;
+    }
+  });/*
+
+  /* board[i].onclick = function() {
+    board[i].innerHTML = player1;
+     if (board[i].innerHTML === player1) {
+      count++;
+      console.log(count);
+    }
+    if (count % 2 === 0) {
+      board[i].innerHTML = player2;
+    } 
+  }; */
+}
