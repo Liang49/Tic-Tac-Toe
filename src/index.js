1bd6af286cdf758ffeb9b0da9fa234c012c246a1
Liang49 <liangzhang2014@gmail.com>
2019-10-02 21:10:32 -0400

    Click Event
    Remove it

--- src/index.js
+++ src/index.js
@@ -4,16 +4,29 @@ var player1 = "O";
 var player2 = "X";
 
 function handleClick(e) {
-  count++;
   console.log(count);
   document.getElementById(e.target.id).innerHTML = player1;
+  if (player1) {
+    count++;
+  }
+  if (count % 2 === 0) {
+    document.getElementById(e.target.id).innerHTML = player2;
+  }
+  document
+    .getElementById(e.target.id)
+    .removeEventListener("click", handleClick);
 }
-
+/*function removeClick(e) {
+  if(document.getElementById(e.target.id).innerHTML = player1 || player2) {
+    
+  }
+} */
 var count = 0;
 
 var board = document.querySelectorAll(".board");
 for (let i = 0; i < board.length; i++) {
   board[i].addEventListener("click", handleClick);
+
   /*board[i].addEventListener("click", function() {
     board[i].innerHTML = player1;
     if (board[i].innerHTML === player1) {
